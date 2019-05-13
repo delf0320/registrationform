d99a8d34bc7cf887d2ef113a56c2a35f7f6212a7
delf <delf.boston@gmail.com>
2019-05-13 14:33:02 +0800

    registration form with validation

---
+++ js/passwordvalidation.js
@@ -0,0 +1,21 @@
+function checkPassword(str)
+  {
+    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    return re.test(str);
+  }
+
+  function checkForm(form)
+  {
+    if(form.password.value != "" && form.password.value == form.confirmpassword.value) {
+      if(!checkPassword(form.confirmpassword.value)) {
+        alert("The password you have entered is not valid!");
+        form.password.focus();
+        return false;
+      }
+    } else {
+      alert("Error: Please check that you've entered and confirmed your password!");
+      form.password.focus();
+      return false;
+    }
+    return true;
+  }
